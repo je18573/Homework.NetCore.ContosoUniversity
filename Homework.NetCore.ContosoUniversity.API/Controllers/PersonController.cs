b22e58a372555dbba8cf915eebec1f3b3aa9f8af
yuhua.wu <je18573@gmail.com>
2019-12-10 22:26:37 +0800

    feat: 新增Person API功能
    - 使用Entity Framework執行動作的API控制器自動產生
    　+ 自動安裝 Microsoft.VisualStudio.Web.CodeGeneration.Design
    - 注入ILogger：使用try...cache區塊補上log紀錄
    　+ 自動安裝 Microsoft.Extensions.Logging.Debug
    - Post：加上ModelState檢查

---
+++ Homework.NetCore.ContosoUniversity.API/Controllers/PersonController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Homework.NetCore.ContosoUniversity.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework.NetCore.ContosoUniversity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+        private readonly ILogger _logger;
+
+        public PersonController(ContosouniversityContext context,
+            ILogger<PersonController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Person
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPerson()
+        {
+            return await _context.Person.ToListAsync();
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(int id)
+        {
+            var person = await _context.Person.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        // PUT: api/Person/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(int id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+
+                _logger.LogError("PutPerson Fail! ", ex);
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Person
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Person.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Id }, person);
+        }
+
+        // DELETE: api/Person/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Person>> DeletePerson(int id)
+        {
+            var person = await _context.Person.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.Person.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return person;
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.Person.Any(e => e.Id == id);
+        }
+    }
+}
