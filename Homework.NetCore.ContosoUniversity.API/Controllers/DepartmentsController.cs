ff827b3d704e652d242d82ce7f91795103cad5fd
yuhua.wu <je18573@gmail.com>
2019-12-13 13:37:19 +0800

    feat: 新增Departments API功能
    1. 基本 CRUD
     - 使用自動程式碼產生器
     - 調整 CUD，改成使用預存程序
    2. 新增 GetDepartmentCourseCount：使用Raw SQL Query 的方式查詢 vwDepartmentCourseCount 檢視表的內容

---
+++ Homework.NetCore.ContosoUniversity.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,123 @@
+﻿using Homework.NetCore.ContosoUniversity.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework.NetCore.ContosoUniversity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+        private readonly ILogger _logger;
+
+        public DepartmentsController(ContosouniversityContext context,
+            ILogger<DepartmentsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartment()
+        {
+            return await _context.Department.ToListAsync();
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return department;
+        }
+
+        // PUT: api/Departments/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment(int id, Department department)
+        {
+            if (id != department.DepartmentId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _context.Database.ExecuteSqlInterpolatedAsync(
+                    $"EXEC [dbo].[Department_Update] {department.DepartmentId}, {department.Name}, {department.Budget}, {department.StartDate}, {department.InstructorId}, {department.RowVersion}"
+                );
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+
+                _logger.LogError("PutCourse Fail! ", ex);
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Departments
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Department>> PostDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            await _context.Database.ExecuteSqlInterpolatedAsync(
+                $"EXEC [dbo].[Department_Insert] {department.Name}, {department.Budget}, {department.StartDate}, {department.InstructorId} "
+            );
+
+            return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Department>> DeleteDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            await _context.Database.ExecuteSqlInterpolatedAsync(
+                $"EXEC [dbo].[Department_Delete] {department.DepartmentId}, {department.RowVersion}"
+            );
+
+            return department;
+        }
+
+        // GET: api/Departments/CourseCount
+        [HttpGet("CourseCount")]
+        public async Task<ActionResult<IEnumerable<VwDepartmentCourseCount>>> GetDepartmentCourseCount()
+        {
+            var data = await _context.VwDepartmentCourseCount.FromSqlRaw("SELECT * FROM VwDepartmentCourseCount").ToListAsync();
+            return data;
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return _context.Department.Any(e => e.DepartmentId == id);
+        }
+    }
+}
