6055681d8634a49b2ca66d5bd7aa42788d1c2e89
yuhua.wu <je18573@gmail.com>
2019-12-13 14:04:39 +0800

    feat: 新增Enrollments, OfficeAssignments API功能

---
+++ Homework.NetCore.ContosoUniversity.API/Controllers/EnrollmentsController.cs
@@ -0,0 +1,113 @@
+﻿using Homework.NetCore.ContosoUniversity.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework.NetCore.ContosoUniversity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+        private readonly ILogger _logger;
+
+        public EnrollmentsController(ContosouniversityContext context,
+            ILogger<EnrollmentsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Enrollments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Enrollment>>> GetEnrollment()
+        {
+            return await _context.Enrollment.ToListAsync();
+        }
+
+        // GET: api/Enrollments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Enrollment>> GetEnrollment(int id)
+        {
+            var enrollment = await _context.Enrollment.FindAsync(id);
+
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return enrollment;
+        }
+
+        // PUT: api/Enrollments/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEnrollment(int id, Enrollment enrollment)
+        {
+            if (id != enrollment.EnrollmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(enrollment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!EnrollmentExists(id))
+                {
+                    return NotFound();
+                }
+
+                _logger.LogError("PutEnrollment Fail! ", ex);
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Enrollments
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Enrollment>> PostEnrollment(Enrollment enrollment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Enrollment.Add(enrollment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEnrollment", new { id = enrollment.EnrollmentId }, enrollment);
+        }
+
+        // DELETE: api/Enrollments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Enrollment>> DeleteEnrollment(int id)
+        {
+            var enrollment = await _context.Enrollment.FindAsync(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Enrollment.Remove(enrollment);
+            await _context.SaveChangesAsync();
+
+            return enrollment;
+        }
+
+        private bool EnrollmentExists(int id)
+        {
+            return _context.Enrollment.Any(e => e.EnrollmentId == id);
+        }
+    }
+}
---
+++ Homework.NetCore.ContosoUniversity.API/Controllers/OfficeAssignmentsController.cs
@@ -0,0 +1,128 @@
+﻿using Homework.NetCore.ContosoUniversity.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework.NetCore.ContosoUniversity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfficeAssignmentsController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+        private readonly ILogger _logger;
+
+        public OfficeAssignmentsController(ContosouniversityContext context,
+            ILogger<OfficeAssignmentsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/OfficeAssignments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OfficeAssignment>>> GetOfficeAssignment()
+        {
+            return await _context.OfficeAssignment.ToListAsync();
+        }
+
+        // GET: api/OfficeAssignments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> GetOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignment.FindAsync(id);
+
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            return officeAssignment;
+        }
+
+        // PUT: api/OfficeAssignments/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOfficeAssignment(int id, OfficeAssignment officeAssignment)
+        {
+            if (id != officeAssignment.InstructorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(officeAssignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!OfficeAssignmentExists(id))
+                {
+                    return NotFound();
+                }
+
+                _logger.LogError("PutOfficeAssignment Fail! ", ex);
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OfficeAssignments
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<OfficeAssignment>> PostOfficeAssignment(OfficeAssignment officeAssignment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.OfficeAssignment.Add(officeAssignment);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (OfficeAssignmentExists(officeAssignment.InstructorId))
+                {
+                    return Conflict();
+                }
+
+                _logger.LogError("PostOfficeAssignment Fail! ", ex);
+
+                throw;
+            }
+
+            return CreatedAtAction("GetOfficeAssignment", new { id = officeAssignment.InstructorId }, officeAssignment);
+        }
+
+        // DELETE: api/OfficeAssignments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> DeleteOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignment.FindAsync(id);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.OfficeAssignment.Remove(officeAssignment);
+            await _context.SaveChangesAsync();
+
+            return officeAssignment;
+        }
+
+        private bool OfficeAssignmentExists(int id)
+        {
+            return _context.OfficeAssignment.Any(e => e.InstructorId == id);
+        }
+    }
+}
